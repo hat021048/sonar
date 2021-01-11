e19ed2cf86418d26730a88afb50d358f729590c3
hat021048 <73167096+hat021048@users.noreply.github.com>
2021-01-11 12:50:18 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,11 @@ function Sonar () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     control.waitMicros(2)
     pins.digitalWritePin(DigitalPin.P0, 1)
-    control.waitMicros(10)
+    control.waitMicros(50)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) / 5
-    serial.writeValue("Dist: ", distance)
+    control.waitMicros(100)
+    distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) / 4
+    basic.showNumber(distance)
 }
 let distance = 0
 basic.showIcon(IconNames.Yes)
