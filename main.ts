d1907ee6f62f00bc8aba506a42330d47275d486f
hat021048 <73167096+hat021048@users.noreply.github.com>
2021-02-04 19:05:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,23 +5,24 @@ function Sonar () {
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P16, 0)
     control.waitMicros(100)
-    distance = pins.pulseIn(DigitalPin.P15, PulseValue.High) / 5.5
+    distance = pins.pulseIn(DigitalPin.P15, PulseValue.High) / 55
 }
 let Speed = 0
 let distance = 0
+basic.showIcon(IconNames.Yes)
 pins.digitalWritePin(DigitalPin.P8, 0)
 pins.digitalWritePin(DigitalPin.P7, 1)
 distance = 5
 basic.forever(function () {
     Sonar()
-    if (distance > 300) {
+    if (distance > 20) {
         Speed = 1023
-    } else if (distance > 200) {
+    } else if (distance > 15) {
         Speed = 800
-    } else if (distance > 100) {
-        Speed = 600
-    } else if (distance > 50) {
-        Speed = 300
+    } else if (distance > 10) {
+        Speed = 500
+    } else if (distance > 5) {
+        Speed = 200
     } else {
         Speed = 0
     }
