a7105b46f370d7f7da2e785840ff597e77351984
hat021048 <73167096+hat021048@users.noreply.github.com>
2021-02-05 16:50:47 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,33 @@
 function Sonar () {
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
     control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
     control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
     control.waitMicros(100)
-    distance = pins.pulseIn(DigitalPin.P15, PulseValue.High) / 55
+    distance = pins.pulseIn(DigitalPin.P13, PulseValue.High) / 35
 }
 let Speed = 0
 let distance = 0
 basic.showIcon(IconNames.Yes)
-pins.digitalWritePin(DigitalPin.P8, 0)
-pins.digitalWritePin(DigitalPin.P7, 1)
 distance = 5
 basic.forever(function () {
     Sonar()
     if (distance > 20) {
-        Speed = 1023
+        Speed = 255
     } else if (distance > 15) {
-        Speed = 800
+        Speed = 120
     } else if (distance > 10) {
-        Speed = 500
+        Speed = 80
     } else if (distance > 5) {
-        Speed = 200
+        Speed = 50
     } else {
         Speed = 0
     }
-    pins.analogWritePin(AnalogPin.P1, Speed)
+    robotbit.MotorRunDual(
+    robotbit.Motors.M1A,
+    Speed,
+    robotbit.Motors.M2A,
+    Speed
+    )
 })
